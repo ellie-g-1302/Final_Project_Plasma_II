51f2ed975521285cceab9018627db0b4ed489a39
Ellie <161966002+ellie-g-1302@users.noreply.github.com>
2026-04-22 15:01:53 -0400

    Update thermal_conductivity.py

--- thermal_conductivity.py
+++ thermal_conductivity.py
@@ -4,19 +4,83 @@ import pandas as pd
 import math
 import thermal
 
-## Data -----------------------------------
-tele = 250 * 11600
-tion = 300 * 1160
-nele = np.linspace(1e21,1e30,100)
+## Data --------------------------------------------------------------------------------
+N = 1000
+tele = np.linspace(0.001,10,N) * 11600 * 1000
+tion = np.linspace(0.001, 10,N) * 11600
+nele = 1e30
 Z = 1
 A = 2.5
 nion = Z * nele
 
+## So we're gonna work at the low temperature, high density limit
 
-thermCon = thermal.Conductivity(tele, tion, nion, nele, Z, A)
-KGMS = thermCon.LeeMoreConductivity("GMS")
-# plt.plot(nele, llGMS[1])
-# plt.xscale("log")
+## Calculating the Data logLambda-------------------------------------------------------
+ls = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).calcLogLambda("ls") for i in range(N)]
+lm = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).calcLogLambda("lm") for i in range(N)]
+gms = [thermal.Conductivity(tele, tion, nion, nele, Z, A).calcLogLambda("gms") for i in range(N)]
+
+## Calculating the Electrical Conductivity ---------------------------------------------
+lsSigmaElectric = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).SpitzerElectricConductivity("ls") for i in range(N)]
+lmSigmaElectric = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).LeeMoreElectricConductivity("lm") for i in range(N)]
+
+## Calculating the Thermal Conductivity ------------------------------------------------
+lsSigmaThermal = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).SpitzerThermalConductivity("ls") for i in range(N)]
+lmSigmaThermal = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).LeeMoreThermalConductivity("lm") for i in range(N)]
+
+## Physical interpretation -------------------------------------------------------------
+MagneticReynoldsLS = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).magneticReynoldsNumber(100, 0.01, "ls") for i in range(N)]
+MagneticReynoldsLM = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).magneticReynoldsNumber(100, 0.01, "lm") for i in range(N)]
+BLS = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).OhmsLaw("ls", 100, 10) for i in range(N)]
+BLM = [thermal.Conductivity(tele[i], tion[i], nion, nele, Z, A).OhmsLaw("lm", 100, 10) for i in range(N)]
+
+## ChatGPT Generated Code for Graphing plots (edited)
+# Create a 2x2 grid of subplots
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+# Top-left subplot
+axs[0, 0].plot(tele, BLS,c="#7CFFCB")
+axs[0, 0].plot(tele, BLM, c="#A2003B")
+axs[0, 0].set_xscale("log")
+axs[0, 0].set_yscale("log")
+axs[0, 0].set_title("Magnetic Field")
+
+# # Top-right subplot
+axs[0, 1].plot(tele, lsSigmaElectric,c="#7CFFCB")
+axs[0, 1].plot(tele, lmSigmaElectric, c="#A2003B")
+axs[0, 1].set_title("Electrical Conductivity")
+axs[0, 1].set_xscale("log")
+axs[0, 1].set_yscale("log")
+
+# # Bottom-left subplot
+axs[1, 0].plot(tele, lsSigmaThermal, c="#7CFFCB")
+axs[1, 0].plot(tele, lmSigmaThermal, c="#A2003B")
+axs[1, 0].set_title("Thermal Conductivity")
+axs[1, 0].set_xscale("log")
+axs[1, 0].set_yscale("log")
+
+
+# # Bottom-right subplot
+axs[1, 1].plot(tele, MagneticReynoldsLS, c="#7CFFCB")
+axs[1, 1].plot(tele, MagneticReynoldsLM, c="#A2003B")
+axs[1, 1].set_title("Magnetic Reynolds")
+axs[1, 1].set_xscale("log")
+axs[1, 1].set_yscale("log")
+
+# Adjust layout to prevent overlap
+plt.tight_layout()
+
+# Show the plots
+plt.show()
+
+# plt.plot(nele, ls, label = "Spizter")
+# plt.plot(nele, lm, label = "LeeMore")
+# plt.plot(nele, gms, label = "GMS")
+# plt.plot(nele, lsSigma, label = "Spitzer Conductivity")
+# plt.plot(nele, lmSigma, label = "Lee-More Conductivity")
+# plt.legend()
+# plt.grid(which="major", color = "#636363")
+# plt.grid(which="minor", linestyle = "dashed", color="#bfbfbf")
 # plt.show()
 
 
